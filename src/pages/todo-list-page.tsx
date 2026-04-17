7e8be953c892485584fc36e75d660bb64377cdac
sangminLee <smlee@takensoft.co.kr>
2026-04-17 17:44:40 +0900

    캐싱매커니즘이해하기1부터하기

--- src/pages/todo-list-page.tsx
+++ src/pages/todo-list-page.tsx
@@ -1,14 +1,18 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
-import { useTodos } from "@/store/todos";
+import { useTodosData } from "@/hooks/quries/use-todos-data";
 
 export default function TodoListPage() {
-    const todos = useTodos();
+    const { data: todos, isLoading, error } = useTodosData();
+
+    if (error) return <div>오류가 발생했습니다.</div>
+    if (isLoading) return <div>로딩 중 입니다.</div>
+
     return (
         <div className="flex flex-col gap-5 p-5">
             <h1 className="text-2xl font-bold">TodoList</h1>
             <TodoEditor />
-            {todos.map((todo) => <TodoItem key={todo.id} {...todo} />)}
+            {todos?.map((todo) => <TodoItem key={todo.id} {...todo} />)}
         </div>
     )
 }
